42129e86effc73bb21501b0e565d6cf5b1863933
Domino Marama <domino@dominodesigns.info>
2009-06-17 14:49:56 +0100

    Cleanup of theme code in gui.py

--- master/primstar/gui.py
+++ master/primstar/gui.py
@@ -24,50 +24,50 @@ from os import name as OSNAME
 import Tkinter
 from binascii import hexlify
 
-def hex_colour(theme_colour):
-	return "#" + hexlify("".join([chr(i) for i in theme_colour[:-1]]))
+def hex_color(theme_color):
+	return "#" + hexlify("".join([chr(i) for i in theme_color[:-1]]))
 
-def float_alpha(theme_colour):
-	return float(theme_colour[3]) / 255.0
+def float_alpha(theme_color):
+	return float(theme_color[3]) / 255.0
 
 class Theme:
 	def __init__(self):
 		ui = Blender.Window.Theme.Get()[0].get('ui')
-		self.action = {'colour':hex_colour(ui.action),
+		self.action = {'color':hex_color(ui.action),
 				'alpha':float_alpha(ui.action)}
 		self.draw_type = ui.drawType
 		self.icon_theme = ui.iconTheme
-		self.menu_back = {'colour':hex_colour(ui.menu_back),
+		self.menu_back = {'color':hex_color(ui.menu_back),
 				'alpha':float_alpha(ui.menu_back)}
-		self.menu_hilite = {'colour':hex_colour(ui.menu_hilite),
+		self.menu_hilite = {'color':hex_color(ui.menu_hilite),
 				'alpha':float_alpha(ui.menu_hilite)}
-		self.menu_item = {'colour':hex_colour(ui.menu_item),
+		self.menu_item = {'color':hex_color(ui.menu_item),
 				'alpha':float_alpha(ui.menu_item)}
-		self.menu_text = {'colour':hex_colour(ui.menu_text),
+		self.menu_text = {'color':hex_color(ui.menu_text),
 				'alpha':float_alpha(ui.menu_text)}
-		self.menu_text_hi = {'colour':hex_colour(ui.menu_text_hi),
+		self.menu_text_hi = {'color':hex_color(ui.menu_text_hi),
 				'alpha':float_alpha(ui.menu_text_hi)}
-		self.neutral = {'colour':hex_colour(ui.neutral),
+		self.neutral = {'color':hex_color(ui.neutral),
 				'alpha':float_alpha(ui.neutral)}
-		self.num = {'colour':hex_colour(ui.num),
+		self.num = {'color':hex_color(ui.num),
 				'alpha':float_alpha(ui.num)}
-		self.outline = {'colour':hex_colour(ui.outline),
+		self.outline = {'color':hex_color(ui.outline),
 				'alpha':float_alpha(ui.outline)}
-		self.popup = {'colour':hex_colour(ui.popup),
+		self.popup = {'color':hex_color(ui.popup),
 				'alpha':float_alpha(ui.popup)}
-		self.setting = {'colour':hex_colour(ui.setting),
+		self.setting = {'color':hex_color(ui.setting),
 				'alpha':float_alpha(ui.setting)}
-		self.setting1 = {'colour':hex_colour(ui.setting1),
+		self.setting1 = {'color':hex_color(ui.setting1),
 				'alpha':float_alpha(ui.setting1)}
-		self.setting2 = {'colour':hex_colour(ui.setting2),
+		self.setting2 = {'color':hex_color(ui.setting2),
 				'alpha':float_alpha(ui.setting2)}
-		self.text = {'colour':hex_colour(ui.text),
+		self.text = {'color':hex_color(ui.text),
 				'alpha':float_alpha(ui.text)}
-		self.text_hi = {'colour':hex_colour(ui.text_hi),
+		self.text_hi = {'color':hex_color(ui.text_hi),
 				'alpha':float_alpha(ui.text_hi)}
-		self.textfield = {'colour':hex_colour(ui.textfield),
+		self.textfield = {'color':hex_color(ui.textfield),
 				'alpha':float_alpha(ui.textfield)}
-		self.textfield_hi = {'colour':hex_colour(ui.textfield_hi),
+		self.textfield_hi = {'color':hex_color(ui.textfield_hi),
 				'alpha':float_alpha(ui.textfield_hi)}
 
 class Root(Tkinter.Tk):
@@ -75,10 +75,10 @@ class Root(Tkinter.Tk):
 		Tkinter.Tk.__init__(self, *args, **kw)
 		if title is not None:
 			self.title( title )
-		self.bg=theme.neutral['colour']
+		self.bg=theme.menu_back['color']
 		# OS specific features
 		if OSNAME in ['nt','mac']:
-			self.attributes("-alpha". theme.neutral['alpha'])
+			self.attributes("-alpha". theme.menu_back['alpha'])
 		# event handling
 		self.focusmodel("passive")
 		self.bind('<FocusOut>', self.focus_out_handler)
@@ -103,16 +103,16 @@ class Root(Tkinter.Tk):
 		self.destroy()
 
 class ModalRoot(Tkinter.Tk):
-	def __init__(self, *args,**kw):
-		Tkinter.Tk.__init__(self, *args, **kw)
+	def __init__(self):
+		Tkinter.Tk.__init__(self)
 		self.overrideredirect(True)
 		self.configure(takefocus=True)
 		if OSNAME == "nt":
 			self.attributes("-topmost", 1)   # Required for Windows
-		self.bg=theme.neutral['colour']
+		self.bg=theme.menu_back['color']
 		# OS specific features
 		if OSNAME in ['nt','mac']:
-			self.attributes("-alpha", theme.neutral['alpha'])
+			self.attributes("-alpha", theme.menu_back['alpha'])
 		# event handling
 		self.focusmodel("active")
 		self.bind('<Escape>', self.destroy_handler)
@@ -130,7 +130,8 @@ class ModalRoot(Tkinter.Tk):
 class Frame(Tkinter.Frame):
 	def __init__(self, *args, **kw):
 		Tkinter.Frame.__init__(self, *args,**kw)
-		self.bg=theme.neutral['colour']
+		self.bg=theme.neutral['color']
+		self.highlightcolor=theme.menu_hilite['color']
 		# OS specific features
 		if OSNAME in ['nt','mac']:
 			self.attributes("-alpha", theme.neutral['alpha'])
