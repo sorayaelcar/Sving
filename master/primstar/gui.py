ac24894a3951cd8e2c76d329a7422e8c42573df3
Domino Marama <domino@dominodesigns.info>
2009-06-17 14:03:06 +0100

    Alpha support added to theme in gui.py

--- master/primstar/gui.py
+++ master/primstar/gui.py
@@ -27,33 +27,57 @@ from binascii import hexlify
 def hex_colour(theme_colour):
 	return "#" + hexlify("".join([chr(i) for i in theme_colour[:-1]]))
 
+def float_alpha(theme_colour):
+	return float(theme_colour[3]) / 255.0
+
 class Theme:
 	def __init__(self):
 		ui = Blender.Window.Theme.Get()[0].get('ui')
-		self.action = hex_colour(ui.action)
+		self.action = {'colour':hex_colour(ui.action),
+				'alpha':float_alpha(ui.action)}
 		self.draw_type = ui.drawType
 		self.icon_theme = ui.iconTheme
-		self.menu_back = hex_colour(ui.menu_back)
-		self.menu_hilite = hex_colour(ui.menu_hilite)
-		self.menu_item = hex_colour(ui.menu_item)
-		self.menu_text = hex_colour(ui.menu_text)
-		self.menu_text_hi = hex_colour(ui.menu_text_hi)
-		self.neutral = hex_colour(ui.neutral)
-		self.num = hex_colour(ui.num)
-		self.outline = hex_colour(ui.outline)
-		self.popup = hex_colour(ui.popup)
-		self.setting = hex_colour(ui.setting)
-		self.setting1 = hex_colour(ui.setting1)
-		self.setting2 = hex_colour(ui.setting2)
-		self.text = hex_colour(ui.text)
-		self.text_hi = hex_colour(ui.text_hi)
-		self.textfield = hex_colour(ui.textfield)
-		self.textfield_hi = hex_colour(ui.textfield_hi)
+		self.menu_back = {'colour':hex_colour(ui.menu_back),
+				'alpha':float_alpha(ui.menu_back)}
+		self.menu_hilite = {'colour':hex_colour(ui.menu_hilite),
+				'alpha':float_alpha(ui.menu_hilite)}
+		self.menu_item = {'colour':hex_colour(ui.menu_item),
+				'alpha':float_alpha(ui.menu_item)}
+		self.menu_text = {'colour':hex_colour(ui.menu_text),
+				'alpha':float_alpha(ui.menu_text)}
+		self.menu_text_hi = {'colour':hex_colour(ui.menu_text_hi),
+				'alpha':float_alpha(ui.menu_text_hi)}
+		self.neutral = {'colour':hex_colour(ui.neutral),
+				'alpha':float_alpha(ui.neutral)}
+		self.num = {'colour':hex_colour(ui.num),
+				'alpha':float_alpha(ui.num)}
+		self.outline = {'colour':hex_colour(ui.outline),
+				'alpha':float_alpha(ui.outline)}
+		self.popup = {'colour':hex_colour(ui.popup),
+				'alpha':float_alpha(ui.popup)}
+		self.setting = {'colour':hex_colour(ui.setting),
+				'alpha':float_alpha(ui.setting)}
+		self.setting1 = {'colour':hex_colour(ui.setting1),
+				'alpha':float_alpha(ui.setting1)}
+		self.setting2 = {'colour':hex_colour(ui.setting2),
+				'alpha':float_alpha(ui.setting2)}
+		self.text = {'colour':hex_colour(ui.text),
+				'alpha':float_alpha(ui.text)}
+		self.text_hi = {'colour':hex_colour(ui.text_hi),
+				'alpha':float_alpha(ui.text_hi)}
+		self.textfield = {'colour':hex_colour(ui.textfield),
+				'alpha':float_alpha(ui.textfield)}
+		self.textfield_hi = {'colour':hex_colour(ui.textfield_hi),
+				'alpha':float_alpha(ui.textfield_hi)}
 
 class Root(Tkinter.Tk):
 	def __init__(self, *args,**kw):
 		Tkinter.Tk.__init__(self, *args, **kw)
-		self.bg=theme.neutral
+		self.bg=theme.neutral['colour']
+		# OS specific features
+		if OSNAME in ['nt','mac']:
+			self.attributes("-alpha". theme.neutral['alpha'])
+		# event handling
 		self.focusmodel("passive")
 		self.bind('<FocusOut>', self.focus_out_handler)
 		self.bind('<FocusIn>', self.focus_in_handler)
@@ -82,8 +106,12 @@ class ModalRoot(Tkinter.Tk):
 		self.overrideredirect(True)
 		self.configure(takefocus=True)
 		if OSNAME == "nt":
-			self.master.wm_attributes("-topmost", 1)   # Make sure window remains on top of all others
-		self.bg=theme.neutral
+			self.attributes("-topmost", 1)   # Required for Windows
+		self.bg=theme.neutral['colour']
+		# OS specific features
+		if OSNAME in ['nt','mac']:
+			self.attributes("-alpha", theme.neutral['alpha'])
+		# event handling
 		self.focusmodel("active")
 		self.bind('<Escape>', self.destroy_handler)
 		self.bind('<Leave>', self.destroy_handler)
@@ -100,7 +128,10 @@ class ModalRoot(Tkinter.Tk):
 class Frame(Tkinter.Frame):
 	def __init__(self, *args, **kw):
 		Tkinter.Frame.__init__(self, *args,**kw)
-		self.bg=theme.neutral
+		self.bg=theme.neutral['colour']
+		# OS specific features
+		if OSNAME in ['nt','mac']:
+			self.attributes("-alpha", theme.neutral['alpha'])
 
 def main():
 	root = ModalRoot()
